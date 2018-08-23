a8cdd92064353794b44843c0fd03c71de89110f6
pursonchen <185297167@qq.com>
2018-08-23 13:52:42 +0000

    seeding user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-		$this->call(MealsTableSeeder::class);
-		$this->call(DishsTableSeeder::class);
-		$this->call(DishTypesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+
     }
 }
