056c75340b3be79d7700b04e543726b13cd7ea1c
pursonchen <185297167@qq.com>
2018-08-24 11:22:14 +0800

    fix modify password error

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,4 +28,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+  
+  public function setPasswordAttribute($value)
+{
+    // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+}
 }
